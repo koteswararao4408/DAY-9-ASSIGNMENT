c5e73ba828d39a0164b57a54943cdd7e82b9011e
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-12 12:40:04 +0530

    Add : Adding Employee Attendance UC1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
 echo "Welcome to Employee Wage Computation Program"
+
+#!/bin/bash -x
+
+randomcheck=$((RANDOM%2))
+ispresent=1
+
+if [ $ispresent -eq $randomcheck ]
+then
+        echo "employee is present"
+else
+        echo "employee is absent"
+fi
