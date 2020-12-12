1fabbab2a755cd346e1ec29c178072ea28782583
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-12 14:20:22 +0530

    Add : Adding Part Time Employee UC3

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -28,3 +28,25 @@ else
         salary=0
 fi
 echo "employee wage :" $salary
+
+
+
+
+#!/bin/bash -x
+
+randomcheck=$((RANDOM%3))
+isFullTime=1
+isPartTime=2
+salary=0
+ratePerHr=20
+if [ $isFullTime -eq $randomcheck ]
+then
+        numOfWorkingHrs=8
+elif [[ $isPartTime -eq $randomcheck ]]
+then
+        numOfWorkingHrs=4
+else
+        numOfWorkingHrs=0
+fi
+salary=$(( ratePerHr*numOfWorkingHrs ))
+echo "Employee wage :" $salary
