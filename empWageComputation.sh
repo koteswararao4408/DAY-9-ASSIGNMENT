67017f2d38d031e055875b2b26afb32a5183556e
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-12 13:51:35 +0530

    Add : Adding Employee Daily Wage UC2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,20 @@ then
 else
         echo "employee is absent"
 fi
+
+
+
+#!/bin/bash -x
+
+randomcheck=$((RANDOM%2))
+ispresent=1
+
+if [ $ispresent -eq $randomcheck ]
+then
+        rateperhr=20
+        numofworkinghrs=8
+        salary=$(( $rateperhr*$numofworkinghrs ))
+else
+        salary=0
+fi
+echo "employee wage :" $salary
