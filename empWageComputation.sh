f95101fa006f6f09bde71f87eb895373c13d2bab
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-22 10:31:48 +0530

    Add:Employee wage using for loop

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,90 +1,5 @@
+#!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        echo "employee is present"
-else
-        echo "employee is absent"
-fi
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        rateperhr=20
-        numofworkinghrs=8
-        salary=$(( $rateperhr*$numofworkinghrs ))
-else
-        salary=0
-fi
-echo "employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-if [ $isFullTime -eq $randomcheck ]
-then
-        numOfWorkingHrs=8
-elif [[ $isPartTime -eq $randomcheck ]]
-then
-        numOfWorkingHrs=4
-else
-        numOfWorkingHrs=0
-fi
-salary=$(( ratePerHr*numOfWorkingHrs ))
-echo "Employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-empcheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-
-case $empcheck in
-
-        $isFullTime)
-        echo "Full Time Employee"
-        empHrs=8
-        ;;
-        $isPartTime)
-        echo "partTime Employee"
-        empHrs=4
-        ;;
-        *)
-        echo "Employee is Absent"
-        empHrs=0
-        ;;
-esac
-
-salary=$(( $ratePerHr * $empHrs ))
-echo "Employee Wage :" $salary
-
-
-
-#!/bin/bash -x
-
 
 isFullTime=1
 isPartTime=2
@@ -92,11 +7,10 @@ salary=0
 ratePerHr=20
 numOfWorkingDays=20
 
-for((day=1;day<=numOfWorkingDays;day++))
+for((day=1;day<=numOfWorkingDays;day++))         #for loop for no.of working days
 do
-        empcheck=$((RANDOM%3))
-        case $empcheck in
-
+       empcheck=$((RANDOM%3))                    #To generate a random value 0 to 2
+       case $empcheck in
            $isFullTime)
            echo "Full Time Employee"
            empHrs=8
@@ -112,8 +26,8 @@ do
        esac
 
 salary=$(( $ratePerHr * $empHrs ))
-echo "Employee Wage per day :" $salary
-totalSalary=$(( $totalSalary + $salary ))
+echo "Employee Wage per day :" $salary         #printing salary per day
+totalSalary=$(( $totalSalary + $salary ))      #Total salary 
 done
 
 echo "Employee wage per month :" $totalSalary
