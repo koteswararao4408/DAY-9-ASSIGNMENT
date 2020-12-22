33acc1046879daa04ac295f1d5064867b94369f0
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-22 10:57:20 +0530

    Add:Wages based on days and hours using fnuction

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,129 +1,7 @@
+#!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        echo "employee is present"
-else
-        echo "employee is absent"
-fi
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        rateperhr=20
-        numofworkinghrs=8
-        salary=$(( $rateperhr*$numofworkinghrs ))
-else
-        salary=0
-fi
-echo "employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-if [ $isFullTime -eq $randomcheck ]
-then
-        numOfWorkingHrs=8
-elif [[ $isPartTime -eq $randomcheck ]]
-then
-        numOfWorkingHrs=4
-else
-        numOfWorkingHrs=0
-fi
-salary=$(( ratePerHr*numOfWorkingHrs ))
-echo "Employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-empcheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-
-case $empcheck in
-
-        $isFullTime)
-        echo "Full Time Employee"
-        empHrs=8
-        ;;
-        $isPartTime)
-        echo "partTime Employee"
-        empHrs=4
-        ;;
-        *)
-        echo "Employee is Absent"
-        empHrs=0
-        ;;
-esac
-
-salary=$(( $ratePerHr * $empHrs ))
-echo "Employee Wage :" $salary
-
-
-
-#!/bin/bash -x
-
-
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-numOfWorkingDays=20
-
-for((day=1;day<=numOfWorkingDays;day++))
-do
-        empcheck=$((RANDOM%3))
-        case $empcheck in
-
-           $isFullTime)
-           echo "Full Time Employee"
-           empHrs=8
-           ;;
-           $isPartTime)
-           echo "partTime Employee"
-           empHrs=4
-           ;;
-           *)
-           echo "Employee is Absent"
-           empHrs=0
-           ;;
-       esac
-
-salary=$(( $ratePerHr * $empHrs ))
-echo "Employee Wage per day :" $salary
-totalSalary=$(( $totalSalary + $salary ))
-done
-
-echo "Employee wage per month :" $totalSalary
-
-
-
-
-#!/bin/bash -x
-
-
+#Constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 SALARY=0
@@ -131,58 +9,13 @@ RATE_PER_HR=20
 MAX_WORKING_DAYS=20
 MAX_WORKING_HRS=60
 
+#Variables
 totalworkingDays=0;
 totalworkingHrs=0;
 
-while [[ $totalworkingHrs -lt $MAX_WORKING_HRS && $totalworkingDays -lt $MAX_WORKING_DAYS ]]
-
-do
-        ((totalworkingDays++))
-        empcheck=$((RANDOM%3))
-
-                case $empcheck in
-
-                        $IS_FULL_TIME)
-                echo "Full Time Employee"
-                empHrs=8
-                ;;
-                $IS_PART_TIME)
-         echo "partTime Employee"
-         empHrs=4
-         ;;
-                *)
-        echo "Employee is Absent"
-         empHrs=0
-         ;;
-
-                esac
-   echo "$empHrs"
-        totalworkingHrs=$(($totalworkingHrs+$empHrs))
-done
-
-totalsalary=$(($totalworkingHrs*$RATE_PER_HR))
-echo "Employee Wage per month :" $totalsalary
-
-
-
-
-#!/bin/bash -x
-
-
-IS_FULL_TIME=1
-IS_PART_TIME=2
-SALARY=0
-RATE_PER_HR=20
-MAX_WORKING_DAYS=20
-MAX_WORKING_HRS=60
-
-totalworkingDays=0;
-totalworkingHrs=0;
-
-function getworkingHrs()
+function getworkingHrs()          #function to get working hours of an employee
 {
-         case $empcheck in
-
+     case $empcheck in
          $IS_FULL_TIME)
          empHrs=8
          ;;
@@ -192,17 +25,16 @@ function getworkingHrs()
          *)
          empHrs=0
          ;;
-
       esac
-echo $empHrs
+      echo $empHrs
 }
 while [[ $totalworkingHrs -lt $MAX_WORKING_HRS && $totalworkingDays -lt $MAX_WORKING_DAYS ]]
-
+                                                                                               #While loop for Maximum working days and hours
 do
         ((totalworkingDays++))
-        empcheck=$((RANDOM%3))
-        empHrs="$(getworkingHrs $empcheck)"
-        totalworkingHrs=$(( $totalworkingHrs + $empHrs ))
+        empcheck=$((RANDOM%3))                   #To generate a random value 0 to 2
+        empHrs="$(getworkingHrs $empcheck)"      #Calling function and checking for random value
+        totalworkingHrs=$(( $totalworkingHrs + $empHrs ))  #Total working hours
 done
 
 totalsalary=$(( $totalworkingHrs * $RATE_PER_HR ))
