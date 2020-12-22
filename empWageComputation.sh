5213a02f8e8576400f9ad543e17feda691bbee1e
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-22 10:19:12 +0530

    Add: Employee wage using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,68 +1,13 @@
+#!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        echo "employee is present"
-else
-        echo "employee is absent"
-fi
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $ispresent -eq $randomcheck ]
-then
-        rateperhr=20
-        numofworkinghrs=8
-        salary=$(( $rateperhr*$numofworkinghrs ))
-else
-        salary=0
-fi
-echo "employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-randomcheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-salary=0
-ratePerHr=20
-if [ $isFullTime -eq $randomcheck ]
-then
-        numOfWorkingHrs=8
-elif [[ $isPartTime -eq $randomcheck ]]
-then
-        numOfWorkingHrs=4
-else
-        numOfWorkingHrs=0
-fi
-salary=$(( ratePerHr*numOfWorkingHrs ))
-echo "Employee wage :" $salary
-
-
-
-
-#!/bin/bash -x
-
-empcheck=$((RANDOM%3))
+empcheck=$((RANDOM%3))                     #To generate a random value 0 to 2
 isFullTime=1
 isPartTime=2
 salary=0
 ratePerHr=20
 
-case $empcheck in
+case $empcheck in                          #switch case for random value
 
         $isFullTime)
         echo "Full Time Employee"
